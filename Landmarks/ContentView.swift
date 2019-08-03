508b26144a0efc5e3122b3efeb121ec59353c4ea
tdrk18 <tdrk18@gmail.com>
2019-08-04 00:00:28 +0900

    create Landmarks project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by tdrk18 on 2019/08/03.
+//  Copyright © 2019 tdrk18. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
